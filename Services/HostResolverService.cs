ad7c021c977e7caf151749c6d795b387ed17b193
= <=>
2026-08-18 13:53:31 -0300

    refactor(Services): Melhora implementação do HostResolverService

--- Services/HostResolverService.cs
+++ Services/HostResolverService.cs
@@ -1,16 +1,19 @@
 using System.Net;
 using System.Net.Sockets;
 using Scanner.Response;
+using Scanner.Models;
+using System.Diagnostics;
 
 namespace Scanner.Services.HostResolver;
 abstract class HostResolverService
 {
-    internal static Result<IPAddress[]> IsValidAddress(string address)
+    // Retorna os endereços IP associados ao DNS ou o próprio IP informado.
+    internal static async Task<Result<IPAddress[]>> IsValidAddress(string address)
     {
         try
         {
             return Result<IPAddress[]>.Ok(
-                Dns.GetHostAddresses(address)
+                await Dns.GetHostAddressesAsync(address) //retorna todos os ipv4 ou ipv6 endereçado a um dns
             );
         }
         catch(SocketException)
@@ -20,21 +23,31 @@ abstract class HostResolverService
             );
         }
     }
-    internal static async Task<Result<string>> PortIsOpen(IPAddress ip, ushort port)
+    // Verifica se uma porta está aberta em um endereço IPv4 ou IPv6.
+    internal static async Task<ScanResult> PortIsOpen(IPAddress ip, ushort port)
     {
-        using TcpClient client = new(); 
+        Stopwatch stopwatch = Stopwatch.StartNew();
+
+        using TcpClient client = new();
+
         try
         {
             await client.ConnectAsync(ip, port);
 
-            return Result<string>.Ok(
-                $"ip: {ip} - porta: {port} aberta."
+            return ScanResult.Create(
+                ip: ip,
+                port: port,
+                duration: stopwatch.Elapsed,
+                status: SocketError.Success
             );
         }
         catch (SocketException ex)
         {
-            return Result<string>.Error(
-                $"ip: {ip} - porta: {port} {ex.Message}"
+            return ScanResult.Create(
+                ip: ip,
+                port: port,
+                duration: stopwatch.Elapsed,
+                status: ex.SocketErrorCode
             );
         }
     }
