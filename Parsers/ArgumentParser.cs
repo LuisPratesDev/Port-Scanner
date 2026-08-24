5a9ad2daaeac241ab723dbc54f414cd5fe65297f
= <=>
2026-08-24 14:21:08 -0300

    refactor(Parsers): remove validação antiga e implementa validação dos endereços e portas

--- Parsers/ArgumentParser.cs
+++ Parsers/ArgumentParser.cs
@@ -1,81 +1,38 @@
+using System.Net;
 using Scanner.Response;
+using Scanner.Services.HostResolver;
+
 namespace Scanner.Parsers;
 internal static class ArgumentParser
 {
-    internal static Result<string[]> ValidateArgument(string[] args)
+    internal static async IAsyncEnumerable<Result<IPAddress[]>> ResolveAddresses(HashSet<string> address)
     {
-        //Verifico se há algum argumento
-        if (args.Length == 0) return Result<string[]>.Error(
-            "Não há nenhum argumento."
-        );
+        HashSet<Task<Result<IPAddress[]>>> results = new();
 
-        //verifico se o primeiro argumento é equivalente ao comando "scan"
-        if (!args[0].Equals("scan", StringComparison.InvariantCultureIgnoreCase)) return Result<string[]>.Error(
-            "O primeiro comando não é scan."
-        );
-
-        //verifica se tem menos que 3 elementos ou mais do que quatro elementos
-        if (args.Length < 3 || args.Length > 4) return Result<string[]>.Error(
-            "Argumentos insuficientes para continuar."
-        );
-
-        //retorna uma tupla com success caso o elemento correspondente ao indice é um número e retorna o valor do número
-        var elementSecondIsValid = IsValidIndex(args, 2) ? TryGetNumberArgument(args, 2) : (success: false, value: 0);
-        var elementThirdIsValid = IsValidIndex(args, 3) ? TryGetNumberArgument(args, 3) : (success: false, value: 0);
-
-        //verifico se existe um elemento correspondente ao indice 2
-        if (
-            IsValidIndex(args, 2) &&
-            !(
-                elementSecondIsValid.success
-            )
-        ) return Result<string[]>.Error(
-            "O terceiro argumento é inválido"
-        );
-
-        /*
-            verifico se existe um elemento correspondente ao indice 3 e
-            verifico se esse elemento é um número e se é maior que elemento no indice 2
-        */
-        if (
-            IsValidIndex(args, 3) &&
-            !(
-                elementThirdIsValid.success &&
-                elementThirdIsValid.value > elementSecondIsValid.value
-            )
-        ) return Result<string[]>.Error(
-            "O quarto argumento é inválido"
-        );
-        
-        
-        //retorna o valor do array formatado corretamente
-        return Result<string[]>.Ok(
-            FormatArguments(args)
-        );
-    }
-    private static string[] FormatArguments(string[] args)
-    {
-        if (IsValidIndex(args, 3)) return new string[]
+        foreach(string result in address)
         {
-          args[0].ToLowerInvariant(),
-          args[1],
-          args[2],
-          args[3]  
-        };
+            results.Add(HostResolverService.IsValidAddress(result));
+        }
 
-        return new string[]
+        while(results.Count > 0)
         {
-            args[0].ToLowerInvariant(),
-            args[1],
-            args[2],
-        };
+            Task<Result<IPAddress[]>> task = await Task.WhenAny(results);
+
+            results.Remove(task);
+
+            Result<IPAddress[]> completedTask = await task;
+
+            yield return completedTask;
+        }
     }
-    private static (bool success, ushort value) TryGetNumberArgument(string[] args, byte index)
+    internal static IEnumerable<Result<ushort>> ValidatePorts(HashSet<string> ports)
     {
-        return (ushort.TryParse(args[index], out ushort value), value);
-    }
-    private static bool IsValidIndex(string[] args, byte index)
-    {
-        return args.Length - 1 >= index;
+        foreach(string port in ports)
+        {
+            if (ushort.TryParse(port, out ushort correctPort)) yield return Result<ushort>.Ok(correctPort);
+            else yield return Result<ushort>.Error(
+                "Porta inválida"
+            );
+        }
     }
 }
