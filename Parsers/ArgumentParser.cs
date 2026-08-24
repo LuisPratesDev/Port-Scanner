9a6c7e9db3902c1b1de56653d926b9331d5731d4
= <=>
2026-08-24 14:22:44 -0300

    docs(Parsers): adiciona comentários ao ArgumentParser

--- Parsers/ArgumentParser.cs
+++ Parsers/ArgumentParser.cs
@@ -5,6 +5,7 @@ using Scanner.Services.HostResolver;
 namespace Scanner.Parsers;
 internal static class ArgumentParser
 {
+    //Retorna todos os endereços que ficarem prontos
     internal static async IAsyncEnumerable<Result<IPAddress[]>> ResolveAddresses(HashSet<string> address)
     {
         HashSet<Task<Result<IPAddress[]>>> results = new();
@@ -25,11 +26,13 @@ internal static class ArgumentParser
             yield return completedTask;
         }
     }
+    //Retorna todas as portas válidas
     internal static IEnumerable<Result<ushort>> ValidatePorts(HashSet<string> ports)
     {
         foreach(string port in ports)
         {
             if (ushort.TryParse(port, out ushort correctPort)) yield return Result<ushort>.Ok(correctPort);
+            
             else yield return Result<ushort>.Error(
                 "Porta inválida"
             );
