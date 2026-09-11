5a2bb8dd391404cf87fd5722cc17b9052e2fd2b3
= <=>
2026-09-11 18:09:40 -0300

    feat(program): adiciona ponto de entrada da aplicação

--- Program.cs
+++ Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using System.Threading.Channels;
+using Scanner.Response;
+using Scanner.UI.View;
+using System.Net;
+
+// O Scanner ainda está em desenvolvimento, mas essa versão já pode ser usada
+
+Channel<Result<IPAddress[]>> channel = Channel.CreateUnbounded<Result<IPAddress[]>>();
+
+using CancellationTokenSource cts = new();
+
+Console.CancelKeyPress += (_, e) =>
+{
+    e.Cancel = true;
+    cts.Cancel();
+};
+
+ScanView scanView = new();
+
+await scanView.RunScanAsync(
+    channel,
+    cts.Token
+);
